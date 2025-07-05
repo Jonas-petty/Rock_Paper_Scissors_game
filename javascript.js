617fa90382a6297774ffca082cc82771415a3477
Jonas Felix <jonaspetty@hotmail.com>
2025-07-05 19:04:06 +0000

    feat: add the game lifecycle

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 window.addEventListener("load", () => {
     const select = document.querySelector("#options");
     const form = document.querySelector("#form");
-    let round = 1
+    let round = 1;
     let computerScore = 0;
     let humanScore = 0;
     let computerSelection = "";
     let humanSelection = "";
-
 
     // Returns one of the options randomly (rock, paper or scissors)
     function getComputerChoice() {
@@ -22,17 +21,42 @@ window.addEventListener("load", () => {
         humanSelection = event.target.value;
     });
 
+    // Play all the 5 rounds of the game
     function playRound(humanChoice, computerChoice) {
-        console.log(round)
+        console.log(round);
+        console.log(`${humanChoice} - ${computerChoice}`);
         if (round <= 5) {
-            console.log(`${humanChoice} - ${computerChoice}`);
-            round++
+            if (humanChoice === "rock" && computerChoice === "scissors") {
+                console.log("you win");
+            } else if (
+                humanChoice === "scissors" &&
+                computerChoice === "paper"
+            ) {
+                console.log("you win");
+            } else if (humanChoice === "paper" && computerChoice === "rock") {
+                console.log("you win");
+            }
+            if (humanChoice === "rock" && computerChoice === "paper") {
+                console.log("you lose");
+            } else if (
+                humanChoice === "scissors" &&
+                computerChoice === "rock"
+            ) {
+                console.log("you lose");
+            } else if (
+                humanChoice === "paper" &&
+                computerChoice === "scissors"
+            ) {
+                console.log("you lose");
+            } else {
+                console.log("draw");
+                round--;
+            }
+
+            round++;
         } else {
-            console.log("The game is over, restart")
+            console.log("The game is over, restart");
         }
-        
-
-
     }
 
     form.addEventListener("submit", (event) => {
