859e24dae0f884237e4a814d3b0fe391bf23c8ce
Jonas Felix <jonaspetty@hotmail.com>
2025-07-05 18:31:14 +0000

    feat: add the game rounds feature

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 window.addEventListener("load", () => {
     const select = document.querySelector("#options");
+    const form = document.querySelector("#form");
+    let round = 1
     let computerScore = 0;
     let humanScore = 0;
+    let computerSelection = "";
+    let humanSelection = "";
+
 
     // Returns one of the options randomly (rock, paper or scissors)
     function getComputerChoice() {
@@ -14,6 +19,25 @@ window.addEventListener("load", () => {
 
     // Returns the options chosen by the user
     select.addEventListener("change", (event) => {
-        const humanSelection = getHumanChoice(event.target.value);
+        humanSelection = event.target.value;
+    });
+
+    function playRound(humanChoice, computerChoice) {
+        console.log(round)
+        if (round <= 5) {
+            console.log(`${humanChoice} - ${computerChoice}`);
+            round++
+        } else {
+            console.log("The game is over, restart")
+        }
+        
+
+
+    }
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     });
 });
